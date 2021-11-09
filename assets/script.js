eb56adabfde2f78b81ffad881aa7438e7ac5ce14
Ian Li <ian.li1023@gmail.com>
2021-11-08 23:12:23 -0700

    commented questions

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-box')
+
+
 startButton.addEventListener('click', startGame)
 
 function startGame() {
@@ -12,6 +14,27 @@ function setNextQuestion() {
 
 }
 
-function selctAnswer() {
+function selectAnswer() {
 
 }
+
+// Kobe was drafted right out of high school in 1996 by what NBA team?
+// Answer: Charlotte Hornets
+
+// How many times was he an All-Star?
+// A: 18 times.
+
+// How many NBA championships did Bryant win?
+// A: Five.
+
+// He played his entire 20-season professional career in the league with what team?
+// A: The Los Angeles Lakers.
+
+// Kobe became accustomed to his new lifestyle and learned to do what?
+// A: Speak fluent Italian.
+
+// What was Kobe Bryant's first NBA Jersey number? 
+// A: 8
+
+// What was the most points Kobe ever scored in a game? 
+// A: 81
